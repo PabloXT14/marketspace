78768564994f554c17644e3cb0eedefa9d7be0fc
Pablo Alan <pabloxt14@gmail.com>
2025-01-08 17:14:03 -0300

    feat:  config phosphor-icons and svg use

--- mobile/src/app.tsx
+++ mobile/src/app.tsx
@@ -7,6 +7,8 @@ import {
 } from '@expo-google-fonts/karla'
 
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
+import { WhatsappLogo } from 'phosphor-react-native'
+import Logo from '@/assets/logo.svg'
 
 import '../global.css'
 
@@ -20,6 +22,10 @@ export default function App() {
     <GluestackUIProvider mode="light">
       {fontsLoaded ? (
         <View className="flex-1 justify-center items-center">
+          <Logo width={100} height={100} />
+
+          <WhatsappLogo size={32} />
+
           <Text className="text-md font-bold text-blue-500">Hello World</Text>
         </View>
       ) : (
